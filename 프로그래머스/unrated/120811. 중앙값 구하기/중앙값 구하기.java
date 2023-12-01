c8fdb94fdf04e8043bddbb69806394c50c85b2d0
KIMGOEUN <148428523+kimgoeun9936@users.noreply.github.com>
2023-12-01 17:08:43 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.03 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int i = 0; i<array.length; i++){
+            for(int j = i; j < array.length; j++){
+                if(array[j] < array[i]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        answer = array[array.length / 2];
+        
+        return answer;
+    }
+}
