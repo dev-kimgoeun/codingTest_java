b53cb5a090ae95414fb6f78b56749cd9cebc9629
KIMGOEUN <ak0150a01@gmail.com>
2024-01-27 00:27:30 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+
+        if(arr1.length > arr2.length){
+            answer = 1;
+        } else if(arr1.length < arr2.length){
+            answer = -1;
+        }else{
+            int num1 = 0;
+            int num2 = 0;
+            for(int i=0; i<arr1.length; i++){
+                num1 += arr1[i];
+                num2 += arr2[i];
+            }
+            answer = (num1 > num2? 1: (num1==num2? 0 : -1));
+        }
+        return answer;
+    }
+}
