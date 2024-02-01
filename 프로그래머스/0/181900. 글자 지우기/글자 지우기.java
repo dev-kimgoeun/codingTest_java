c3e9a21b3d48bef1935fe96c87210324d1195dba
KIMGOEUN <ak0150a01@gmail.com>
2024-02-02 00:02:41 +0900

    [level 0] Title: 글자 지우기, Time: 1.60 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        String [] list = my_string.split("");
+        
+        for(int i=0; i<indices.length; i++){
+            list[indices[i]] = "";
+        }
+        for(String x : list) answer +=x;
+        return answer;
+    }
+}
