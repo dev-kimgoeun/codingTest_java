d80b7e167fa4a36bdbfa66b8481da9724a100263
KIMGOEUN <ak0150a01@gmail.com>
2024-03-22 20:04:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char tctr = str.charAt(0);
+        System.out.println((int)tctr);
+    }
+}
