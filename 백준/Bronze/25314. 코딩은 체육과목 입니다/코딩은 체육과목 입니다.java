05bf44b17cc812e682e213bc06120ff89eec8e16
KIMGOEUN <ak0150a01@gmail.com>
2024-03-23 01:32:19 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int re = n/4;
+        for(int i=0; i<re; i++){
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
