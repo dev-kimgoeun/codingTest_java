e925f5f967081e031c42a465f15bdd16da6a1401
KIMGOEUN <ak0150a01@gmail.com>
2024-03-23 20:30:23 +0900

    [Bronze IV] Title: 영수증, Time: 220 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            int amount = sc.nextInt();
+            int num = sc.nextInt();
+            sum += amount*num;
+        }
+        if(total == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
