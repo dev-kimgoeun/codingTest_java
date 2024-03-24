8e61e552146f2f5cf6e67820b3b485d7761542b8
KIMGOEUN <ak0150a01@gmail.com>
2024-03-24 18:32:07 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 236 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(b>a) System.out.print("-1");
+        else{
+            int x = (a+b)/2;
+            int y = (a-b)/2;
+            if(x+y == a && x-y ==b){
+                System.out.println(x+" "+y);
+            }else{
+                System.out.print("-1");
+            }
+        }
+    }
+}
