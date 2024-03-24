d795282020bc7dcaf2e12608441e24ba13528fec
KIMGOEUN <ak0150a01@gmail.com>
2024-03-24 17:40:29 +0900

    [Bronze IV] Title: 녹색거탑, Time: 200 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = {1,2,4,8,16,32};
+        System.out.println(a[n]);
+    }
+}
