c5b1beaa45fd2abd5b4aca12aa6fa8e494268c56
KIMGOEUN <ak0150a01@gmail.com>
2024-03-25 02:52:32 +0900

    [Bronze III] Title: 오븐 시계, Time: 232 ms, Memory: 18432 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+        
+        h += t/60;
+        m += t%60;
+        if(m>59){
+            h++;
+            m-=60;
+        }
+        if(h>23){
+            h-=24;
+        }
+        System.out.println(h+" "+m);
+    }
+}
