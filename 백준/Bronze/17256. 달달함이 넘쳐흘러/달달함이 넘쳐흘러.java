435362ee0ea05498d35d1801d73a2dfadff94044
KIMGOEUN <ak0150a01@gmail.com>
2024-07-14 22:04:49 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.Math.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a3 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int a1 = sc.nextInt();
+        sc.nextLine();
+        int c1 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int c3 = sc.nextInt();
+
+        System.out.println(c1-a1);
+        System.out.println(c2/a2);
+        System.out.println(c3-a3);
+    }
+}
