8789f4cd4efd1c45a479edd3a6e192d5fbbc13a3
KIMGOEUN <ak0150a01@gmail.com>
2024-07-15 02:42:30 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        if("SONGDO".equals(str)){
+            System.out.println("HIGHSCHOOL");
+        }else if("CODE".equals(str)){
+            System.out.println("MASTER");
+        }else if("2023".equals(str)){
+            System.out.println("0611");
+        }else if("ALGORITHM".equals(str)){
+            System.out.println("CONTEST");
+        }
+    }
+}
