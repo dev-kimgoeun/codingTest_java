a8a37fe83575e6c5ce19a87db2b088d7f82d65ac
KIMGOEUN <ak0150a01@gmail.com>
2024-07-14 22:53:26 +0900

    [Bronze V] Title: Site Score, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println((a*56) + (b*24) + (c*14) + (d*6));
+    }
+}
