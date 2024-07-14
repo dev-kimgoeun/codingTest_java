ef32a1ead26ebc274458b1afe55c8eb2f39146fb
KIMGOEUN <ak0150a01@gmail.com>
2024-07-15 00:58:58 +0900

    [Bronze V] Title: 초코바, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int money = a*100;
+        if(money >= b){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
