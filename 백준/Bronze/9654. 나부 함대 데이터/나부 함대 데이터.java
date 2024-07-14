650ffff5b3d99c005275af21b72af051c705f495
KIMGOEUN <ak0150a01@gmail.com>
2024-07-14 19:13:28 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 100 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+        System.out.print("N2 Bomber      Heavy Fighter  Limited    21        \n");
+        System.out.print("J-Type 327     Light Combat   Unlimited  1         \n");
+        System.out.print("NX Cruiser     Medium Fighter Limited    18        \n");
+        System.out.print("N1 Starfighter Medium Fighter Unlimited  25        \n");
+        System.out.print("Royal Cruiser  Light Combat   Limited    4         \n");
+
+    }
+}
