e74c35cac1a516c1cbc96e7bc3d1f18bbdf16c47
KIMGOEUN <ak0150a01@gmail.com>
2024-07-16 21:58:07 +0900

    [Bronze IV] Title: Patyki, Time: 176 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/8723. Patyki/Patyki.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a * a == (b * b) + (c * c) || b * b == (a * a) + (c * c) || c * c == (a * a) + (b * b)) System.out.println(1);
+        else if ((a*a) == (b*b) && (b*b) == (c*c)) System.out.println(2);
+        else System.out.println(0);
+    }
+}
