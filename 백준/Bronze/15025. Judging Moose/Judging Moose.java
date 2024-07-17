aec5286ec47818bcba45e3a4506719aa81c48f66
KIMGOEUN <ak0150a01@gmail.com>
2024-07-17 20:27:01 +0900

    [Bronze IV] Title: Judging Moose, Time: 224 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a==0 && b == 0){
+            System.out.println("Not a moose");
+         }else if(a == b){
+            System.out.println("Even " + (a+b));
+        }else {
+            System.out.println("Odd " + (Math.max(a,b))*2);
+        }
+    }
+}
